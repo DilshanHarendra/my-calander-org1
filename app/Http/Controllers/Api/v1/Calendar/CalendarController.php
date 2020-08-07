2e9e349a6885dca8b21a3fe7e50e8c0225577546
sheyDev <52515689+sheyDev@users.noreply.github.com>
2020-08-07 15:31:10 +1000

    models

---
+++ app/Http/Controllers/Api/v1/Calendar/CalendarController.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\v1\Calendar;
+use App\Calendar;
+use App\Repositories\Calendar\CalendarRepositoryInterface;
+
+class CalendarController
+{
+    private $calendarRepository;
+
+    public function __construct(CalendarRepositoryInterface $calendarRepository)
+    {
+        $this->calendarRepository = $calendarRepository;
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function delete(Request $request, $id)
+    {
+
+    }
+
+}
