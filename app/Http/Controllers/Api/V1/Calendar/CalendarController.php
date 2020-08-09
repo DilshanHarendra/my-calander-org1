8d18b4438f5ecdc35a77b4be6c7ae65a2016416f
Yoosuf <mayoosuf@gmail.com>
2020-08-09 12:01:36 +0530

    updated config, and added new models

--- app/Http/Controllers/Api/V1/Calendar/CalendarController.php
+++ app/Http/Controllers/Api/V1/Calendar/CalendarController.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace App\Http\Controllers\Api\v1\Calendar;
+namespace App\Http\Controllers\Api\V1\Calendar;
 use App\Calendar;
 use App\Repositories\Calendar\CalendarRepositoryInterface;
 
