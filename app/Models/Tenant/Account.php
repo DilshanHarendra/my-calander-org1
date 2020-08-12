71479bb784923ca2d0efc5c0dd1f49fecdae68eb
Yoosuf <mayoosuf@gmail.com>
2020-08-12 13:44:33 +0530

    subscriptions

--- app/Models/Tenant/Account.php
+++ app/Models/Tenant/Account.php
@@ -6,12 +6,30 @@ use App\Models\Calendar\Calendar;
 use App\Models\Event\Event;
 use App\Traits\Hashidable;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Cashier\Billable;
 
 class Account extends Model
 {
     use Hashidable;
+    use Billable;
+
 
     protected $table = 'accounts';
+
+
+    protected $fillable = [
+        'name',
+        'account_type',
+        'stripe_id',
+        'card_brand',
+        'card_last_four',
+        'trial_ends_at',
+        'billing_email',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     public function users()
     {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        Cashier::ignoreMigrations();
+
+
         $this->app->bind(
             'App\Repositories\Account\AccountRepositoryInterface',
             'App\Repositories\Account\AccountEloquentRepository');
